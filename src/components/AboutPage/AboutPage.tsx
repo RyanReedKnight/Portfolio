ace1d6a4730b55f9f0a129892166fd3a5d42eb78
RyanReedKnight <ryanreedknight@gmail.com>
2023-03-13 16:34:37 -0700

    Minor modification to component formating per personal preference

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -3,10 +3,12 @@ import styles from './AboutPage.module.css';
 
 interface AboutPageProps {}
 
-const AboutPage: FC<AboutPageProps> = () => (
-  <div className={styles.AboutPage} data-testid="AboutPage">
-    AboutPage Component
-  </div>
-);
+const AboutPage: FC<AboutPageProps> = () => {
+
+  return (
+    <div className={styles.AboutPage} data-testid="AboutPage">
+      AboutPage Component
+    </div>
+)};
 
 export default AboutPage;
