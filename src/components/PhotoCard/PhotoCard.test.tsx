cf57106a3a43240d5e0cc702af783f6f206b1732
RyanReedKnight <ryanreedknight@gmail.com>
2023-04-26 21:02:28 -0700

    Rounded corners and other misc aesthetic tweaks

--- src/components/PhotoCard/PhotoCard.test.tsx
+++ src/components/PhotoCard/PhotoCard.test.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+//import '@testing-library/jest-dom/extend-expect';
 import PhotoCard from './PhotoCard';
+import Photo from '../../models/Photo';
+
+
 
 describe('<PhotoCard />', () => {
+
+  const dummyPhoto:Photo = {
+    title:"photo-title",
+    description: "photo description",
+    location: "photo location",
+  }
+
   test('it should mount', () => {
-    render(<PhotoCard />);
+    render(<PhotoCard photoModel={dummyPhoto}/>);
     
     const photoCard = screen.getByTestId('PhotoCard');
 
