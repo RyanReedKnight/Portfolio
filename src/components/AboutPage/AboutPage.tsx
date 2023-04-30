4e7761722456a70e7d65c8bb40bbba820534ec7d
RyanReedKnight <ryanreedknight@gmail.com>
2023-04-29 18:53:03 -0700

    Changed about page content.

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -9,12 +9,12 @@ const AboutPage: FC<AboutPageProps> = () => {
     <div className={styles.AboutPage} data-testid="AboutPage">
       <h2> Photo Portfilio Web App</h2>
       <p> This is a web application I use to display photos I have taken. The front end which you are viewing now was built with React and TypeScript,
-        and is hosted on an AWS S3 bucket, though in the future I hope to serve it via Nginx. The back-end is built with Spring Boot and is hosted 
+        and served out of an EC2 instance via Nginx. The back-end is built with Spring Boot and is hosted 
         on an AWS EC2 instance, with a mounted ESB drive to store the photos. The EC2 instance is connected to an AWS RDS database, 
         where records pertaining to each photo are stored. There is also an admin portal I use to upload and/or delete photos. In developing this web application, 
         I learned how to read and write image files to a drive with Java, how to mount an external drive to a server for storage, 
-        how to use axios to send and receive image files from a client to a server, 
-        and the basics of using the React library.
+        how to use axios to send and receive image files from a client to a server, the basics of using the React library,
+        how to serve a website out of Nginx, and how to use Nginx as a reverse proxy.
       </p>
       <p>Relevant links</p>
       <ul>
