dde6028273fbaf7c9ab4e40c1152372eb5e81dee
RyanReedKnight <ryanreedknight@gmail.com>
2023-04-30 15:53:14 -0700

    About page typo's fixed

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -10,7 +10,7 @@ const AboutPage: FC<AboutPageProps> = () => {
       <h2> Photo Portfilio Web App</h2>
       <p> This is a web application I use to display photos I have taken. The front end which you are viewing now was built with React and TypeScript,
         and served out of an EC2 instance via Nginx. The back-end is built with Spring Boot and is hosted 
-        on an AWS EC2 instance, with a mounted ESB drive to store the photos. The EC2 instance is connected to an AWS RDS database, 
+        on an AWS EC2 instance, with a mounted EBS drive to store the photos. The EC2 instance is connected to an AWS RDS database, 
         where records pertaining to each photo are stored. There is also an admin portal I use to upload and/or delete photos. In developing this web application, 
         I learned how to read and write image files to a drive with Java, how to mount an external drive to a server for storage, 
         how to use axios to send and receive image files from a client to a server, the basics of using the React library,
@@ -20,7 +20,7 @@ const AboutPage: FC<AboutPageProps> = () => {
       <ul>
         <li><a className={styles.linkStyle} href="https://github.com/RyanReedKnight/PortfolioBackend" target="_blank">Repositiry for the backend of the application.</a></li>
         <li><a className={styles.linkStyle} href="https://github.com/RyanReedKnight/Portfolio" target="_blank">Repository for the front end of the application.</a></li>
-        <li><a className={styles.linkStyle} href="https://github.com/RyanReedKnight/portfolio-admin" target="_blank">Repository for the admin portal</a></li>
+        <li><a className={styles.linkStyle} href="https://github.com/RyanReedKnight/portfolio-admin" target="_blank">Repository for the admin portal.</a></li>
       </ul>
     </div>
 )};
